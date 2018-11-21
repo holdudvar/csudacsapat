ea684a6ec1c6adefa9eaf57543cd31a15b018035
bkitti <45210798+bkitti@users.noreply.github.com>
2018-11-21 14:23:36 +0100

    Add files via upload

---
+++ js/kittiszamologep.js
@@ -0,0 +1,7 @@
+function plusz() {document.szamologep.eredmeny.value = document.szamologep.egy.value * 1 + document.szamologep.ketto.value * 1}
+
+function minusz() {document.szamologep.eredmeny.value = document.szamologep.egy.value - document.szamologep.ketto.value}
+
+function szor() {document.szamologep.eredmeny.value = document.szamologep.egy.value * document.szamologep.ketto.value}
+
+function oszt() {document.szamologep.eredmeny.value = document.szamologep.egy.value / document.szamologep.ketto.value}
