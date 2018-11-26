cd42a53a836f8ae0012c5c639f432929137381cf
pallerpet1402@gmail.com <pallerpet1402@gmail.com>
2018-11-26 08:52:36 +0100

    Good css, bad js

---
+++ js/petiszamologep2.js
@@ -0,0 +1,25 @@
+function addNull() {
+  var pole1 = '';
+  pole1 = pole1.concat('hello');
+  document.querySelector('#result').innerHTML = 15;
+}
+
+/*
+function petiszamologep() {
+
+  var ertek2 = ;
+  var muv = document.querySelector('#operator').value;
+  var eredmeny;
+
+  if (muv === 'add') {
+    eredmeny = ertek1 + ertek2;
+  } else if (muv === 'substract') {
+    eredmeny = ertek1 - ertek2;
+  } else if (muv === 'divide') {
+    eredmeny = ertek1 * ertek2;
+  } else if (muv === 'multiply') {
+    eredmeny = ertek1 / ertek2;
+  }
+  document.querySelector('#result').innerHTML = eredmeny;
+}
+*/
